bc45ccaeac5e9af75bd780ae3dd942cad08f168b
xarxax <llem95_bcn@hotmail.com>
2018-11-21 13:23:25 +0100

    rename

---
+++ extractor.sh
@@ -0,0 +1,21 @@
+#rdf obtained in https://web.archive.org/web/20170303013959/http://dmoz.org/rdf.html
+
+
+# we extract info from the rdf base file
+
+
+
+#this will turn our rdf to a simpler file format
+#python rdfToPairs.py
+
+
+
+#this limits our results to the final dataset we will work onto
+total=$(wc -l catAndUrl.txt) #get the total number of lines (websites)
+python chooseNPages.py  $total 1000 # last parameter is number of webs we want
+
+echo '----------------BEGINNING WEBSITE EXTRACTION----------------'
+#now we must extract the websites
+rm -rf ./dataset/*    #clean previously extracted
+cd htmlExtraction
+scrapy crawl htmlExtractor
--- htmlExtraction/htmlExtraction/spiders/htmlExtractor.py
+++ htmlExtraction/htmlExtraction/spiders/htmlExtractor.py
@@ -1,30 +1,41 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy_splash import SplashRequest
 
 
 
 class htmlExtractor(scrapy.Spider):
     name = 'htmlExtractor'
-
+    custom_settings = {
+        'DOWNLOAD_DELAY': 2,
+        'CONCURRENT_REQUESTS' :100,
+        'REACTOR_THREADPOOL_MAXSIZE' : 20,
+        'LOG_LEVEL' : 'INFO',
+        'COOKIES_ENABLED' : False,
+        'AJAXCRAWL_ENABLED' : True
+    }
     def start_requests(self):
+
         input = open('../catAndUrlChosen.txt','r')
         extractionUrls= []
         for i,line in enumerate(input):
             line = line.split(',')
             cat = line[0]
-            url = ''.join(line[1:])#rest
+            url = ''.join(line[1:]).replace('\n','')#rest
+            print(url)
             extractionUrls.append((cat,url))
             #if i > 20:
-            #    break
-
+            #    br
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
         for cat,url in extractionUrls:
-            yield scrapy.Request(url=url,callback=self.parse,meta = {'cat':cat})
+            yield SplashRequest(url=url,callback=self.parse,meta = {'cat':cat},
+            headers=headers,args={'wait': '1'})
 
     def parse(self,response):
         content = response.body
         url = response.url
         cat =  str(response.meta['cat'])
-        filename = '../dataset/'+str(cat) + '_' + str(url).replace('/','_')
+        filename = '../dataset/'+str(cat) + '_' + str(url[:70]).replace('/','_')
         with open(filename,'wb') as f:
             f.write(str(cat)+ '\n')
             f.write(str(url)+ '\n')
