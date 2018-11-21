bf6e2b89e3bca90fe15bb3736571e1c47f2b5123
xarxax <llem95_bcn@hotmail.com>
2018-11-21 21:14:26 +0100

    small addition

--- featureExtract.sh
+++ featureExtract.sh
@@ -1,3 +1,7 @@
 #creates folder and creates text
 rm -rf datasetFeatures/*
+
+echo '----------------EXTRACTING TEXT FEATURES----------------'
 python extractText.py
+echo '----------------EXTRACTING URL FEATURES----------------'
+echo '----------------EXTRACTING IMAGE FEATURES----------------'
