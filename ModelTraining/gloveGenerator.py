d6459259032d420c1428b11e737ef46bd1d0959e
xarxax <xarxax.dev@gmail.com>
2019-02-19 13:01:31 +0100

    processed glove in chunks

--- ModelTraining/gloveGenerator.py
+++ ModelTraining/gloveGenerator.py
@@ -6,7 +6,8 @@ if not os.path.exists('gloveDataset'):
 
 
 i = int(sys.argv[1])
-
+total = i
+pathContentVectors=[]
 for folderPath in glob.iglob('tokenizedDataset/*'):
     if i <=0:
         print('Reached limit established')
@@ -14,34 +15,38 @@ for folderPath in glob.iglob('tokenizedDataset/*'):
     i-=1
     print(folderPath)
     file = open(folderPath +'/text.txt','r')
-    urlfile= open(folderPath+'/url.txt')
+    #urlfile= open(folderPath+'/url.txt')
     content = file.read()
     content =ast.literal_eval(content)
-    wordVectors = []
-    with open("glove.840B.300d.txt") as infile:
-        for line in infile:
-            vector =line.split()
-            word = vector[0]
-            if vector[1] =='name@domain.com':
-                del vector[1]
+    pathContentVectors+=[[folderPath,content,[]]]
+    if i%5000==0:
+        #read through the glove txt and save the documents
+        #glove represented
+        print('Reading the Common Crawl file. Number of documents processed:')
+        print(total -i)
+        with open("glove.840B.300d.txt") as infile:
+            for line in infile:
+                vector =line.split()
+                word = vector[0]
+                #skip weird cases
+                if vector[1] =='name@domain.com':
+                    del vector[1]
+                if(len(vector) > 301):
+                    continue
+                #otherwise we turn the 1k documents to wordvecs
+                #print(len(pathContentVectors))
+                #print(pathContentVectors[1])
 
-            if(len(vector) > 301):
-                #print('weird shit happened')
-                #print(vector)
-                continue
-            if word in content:
-                wordVectors += [vector]
-            #print(line.split())
-            #print(len(line.split()))
-            #break
-    #print(wordVectors)
-    #break
-    newFilePath = folderPath.replace('tokenizedDataset/','gloveDataset/',1)
-    if not os.path.exists(newFilePath):
-        os.makedirs(newFilePath)
-    #write text
-    with open(newFilePath+'/text.txt', 'w') as f:
-        f.write(str(wordVectors))
-    with open(newFilePath + '/url.txt','w') as f:
-        f.write(urlfile.read())
-    #we must also write the urls
+                for _,content,vectors in pathContentVectors:
+                    if word in content:
+                        vectors += [vector]
+        #now we save all the wordVectors
+        print('Finished reading the file. Creating now new files')
+        for path,content,vectors in pathContentVectors:
+            newFilePath = path.replace('tokenizedDataset/','gloveDataset/',1)
+            if not os.path.exists(newFilePath):
+                os.makedirs(newFilePath)
+            with open(newFilePath+'/text.txt', 'w') as f:
+                f.write(str(vectors))
+        #now we remove the documents already processed
+        pathContentVectors=[]
