ae029f07e4e36531dd322449a2daaecc1878866d
xarxax <xarxax.dev@gmail.com>
2019-02-25 16:26:02 +0100

    added lexVec

--- ModelTraining/lexVexGenerator.py
+++ ModelTraining/lexVexGenerator.py
@@ -1,8 +1,8 @@
 import glob,os,sys,ast
 
 
-if not os.path.exists('gloveDataset'):
-    os.makedirs('gloveDataset')
+if not os.path.exists('lexVecDataset'):
+    os.makedirs('lexVecDataset')
 
 
 i = int(sys.argv[1])
@@ -22,13 +22,11 @@ for folderPath in glob.iglob('tokenizedDataset/*'):
     if i%5000==0:#more than 10k files make python just stop when opening the big file
         #read through the glove txt and save the documents
         #glove represented
-        if total -i < 25001:
-            pathContentVectors=[]
-            continue
         print('Reading the Common Crawl file. Number of documents processed:')
         print(total -i)
-        with open("glove.840B.300d.txt") as infile:
+        with open("lexVec58Bvectors300.txt") as infile:
             #print('begin getting words')
+
             for line in infile:
                 vector =line.split()
                 word = vector[0]
@@ -39,7 +37,6 @@ for folderPath in glob.iglob('tokenizedDataset/*'):
                 if(len(vector) > 301):
                     continue
                 #otherwise we turn the 1k documents to wordvecs
-                #print(len(pathContentVectors))
                 #print(pathContentVectors[1])
                 #print('adding content:')
                 for _,content,vectors in pathContentVectors:
@@ -49,7 +46,7 @@ for folderPath in glob.iglob('tokenizedDataset/*'):
         #now we save all the wordVectors
         print('Finished reading the file. Creating now new files')
         for path,content,vectors in pathContentVectors:
-            newFilePath = path.replace('tokenizedDataset/','gloveDataset/',1)
+            newFilePath = path.replace('tokenizedDataset/','lexVecDataset/',1)
             if not os.path.exists(newFilePath):
                 os.makedirs(newFilePath)
             with open(newFilePath+'/text.txt', 'w') as f:
@@ -60,7 +57,7 @@ for folderPath in glob.iglob('tokenizedDataset/*'):
 ###
 print('Reading the Common Crawl file. Number of documents processed:')
 print(total -i)
-with open("glove.840B.300d.txt") as infile:
+with open("lexVec58Bvectors300.txt") as infile:
     for line in infile:
         vector =line.split()
         word = vector[0]
@@ -78,7 +75,7 @@ with open("glove.840B.300d.txt") as infile:
 #now we save all the wordVectors
 print('Finished reading the file. Creating now new files')
 for path,content,vectors in pathContentVectors:
-    newFilePath = path.replace('tokenizedDataset/','gloveDataset/',1)
+    newFilePath = path.replace('tokenizedDataset/','lexVecDataset/',1)
     if not os.path.exists(newFilePath):
         os.makedirs(newFilePath)
     with open(newFilePath+'/text.txt', 'w') as f:
