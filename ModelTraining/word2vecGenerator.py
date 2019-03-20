175a187a7765f1112012e462c50999ff21406054
xarxax <xarxax.dev@gmail.com>
2019-03-20 14:57:37 +0100

    no repeating work, now warns while loading WE

--- ModelTraining/word2vecGenerator.py
+++ ModelTraining/word2vecGenerator.py
@@ -1,5 +1,15 @@
 import glob,os,sys,ast,gensim
 
+def sumColumn(m, column):
+    total = 0
+    for row in range(len(m)):
+        total += m[row][column]
+    return total
+
+
+print('Loading model')
+
+
 model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
 
 if not os.path.exists('word2vecDataset'):
@@ -8,12 +18,17 @@ if not os.path.exists('word2vecDataset'):
 i = int(sys.argv[1])
 total = i
 pathContentVectors=[]
+counter=0
 for folderPath in glob.iglob('tokenizedDataset/*'):
     if i <=0:
         print('Reached limit established')
         break
     i-=1
-    print(folderPath)
+    counter+=1
+    if os.path.exists(folderPath.replace('tokenizedDataset/','word2vecDataset/',1)):
+        i+=1#effectively skip the iteration
+        continue
+    #print(folderPath)
     file = open(folderPath +'/text.txt','r')
     #urlfile= open(folderPath+'/url.txt')
     content = file.read()
@@ -28,11 +43,17 @@ for folderPath in glob.iglob('tokenizedDataset/*'):
         except:
             print('word \'' + str(w) + '\' not found in word2vec')
             pass
+    if len(vectors) < 1:
+        print('Skipping \"' + str(folderPath) + '\" due to no vectors.')
+        continue
     #vectors = [model[w] for w in content]
     newFilePath = folderPath.replace('tokenizedDataset/','word2vecDataset/',1)
     if not os.path.exists(newFilePath):
         os.makedirs(newFilePath)
     with open(newFilePath+'/text.txt', 'w') as f:
+        vectors = [i[1:] for i in vectors]
+        vectors = [[float(i) for i in j] for j in vectors]
+        vectors = [sumColumn(vectors,i)for i in range(0,len(vectors[0]))]
         f.write(str(vectors))
     #print(str(vectors))
 
