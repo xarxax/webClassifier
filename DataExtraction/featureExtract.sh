b1d3bdeaeeb3d495bafe4f915714756619f490b2
xarxax <xarxax.dev@gmail.com>
2019-03-30 14:38:47 +0100

    final Data extraction versionon

--- DataExtraction/featureExtract.sh
+++ DataExtraction/featureExtract.sh
@@ -5,7 +5,5 @@ numberToExtract=1112222
 echo 'extracting '$numberToExtract
 echo '----------------EXTRACTING TEXT FEATURES----------------'
 python3 extractText.py $numberToExtract
-echo '----------------EXTRACTING URL FEATURES----------------'
+#echo '----------------EXTRACTING URL FEATURES----------------'
 #python3 extractUrls.py $numberToExtract
-echo '----------------EXTRACTING IMAGE FEATURES----------------'
-#python extractImgs.py $numberToExtract
